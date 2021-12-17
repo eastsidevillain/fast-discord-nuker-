553e6540e0ed55de0a9b652289f8abf48bb3319c
THUSKY <91448412+9ZE@users.noreply.github.com>
2021-12-17 05:39:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-# dont skid
-
 from concurrent.futures import ThreadPoolExecutor
 from requests_futures.sessions import FuturesSession
 import os, random
+from colorama import Style, Fore
 
-os.system("cls & mode 80, 23")
+
+c = Fore.RESET
+os.system("cls & mode 80, 23 & title PHOBOS!")
 token, guild = input("TOKEN: "), input("GUILD: ")
-members = open("members.txt")
-os.system("cls & mode 80, 23")
+
+with open("members.txt") as f:
+    members = f.readlines()
 
 
 def mass_ban(members):
@@ -18,7 +20,7 @@ def mass_ban(members):
                 "https://discord.com/api/v{}/guilds/{}/bans/{}".format(
                     random.randint(6, 9), guild, members
                 ),
-                headers={"Authorization": f"Bot {token}"},
+                headers={"Authorization": "Bot" + token},
             ),
         )
     except:
@@ -28,11 +30,12 @@ def mass_ban(members):
 if __name__ == "__main__":
     threads = []
 
-    with ThreadPoolExecutor(max_workers=110) as executor:
+    with ThreadPoolExecutor() as executor:
 
         for m in members:
 
             threads.append(executor.submit(mass_ban, m))
-            
-            
-            print(f"Succesfully Banned; " + m)
+
+            print(
+                f"{Fore.CYAN}{Style.BRIGHT}{Style.DIM}Succesfully Banned -> ; {c} " + m
+            )
