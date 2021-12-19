1c0822396b4606e98bdc5b9f7e25b9ac04b59b70
THUSKY <91448412+eastsidevillain@users.noreply.github.com>
2021-12-18 21:26:53 -0500

    Create scrape.py

---
+++ scrape.py
@@ -0,0 +1,28 @@
+import discord, os
+from discord.ext import commands
+
+os.system("cls & mode 80, 23")
+f = open("members.txt", "w")
+f.close()
+
+
+TOKEN = input(f"BOT TOKEN -> ")
+os.system("cls & mode 80, 23")
+
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix=None, intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}.")
+    GUILD = int(input("\nGuild ID to Scrape -> "))
+    os.system("cls")
+    id = client.get_guild(GUILD)
+    y = open("members.txt", "a")
+    for member in id.members:
+        y.write(f"\n{member.id}")
+    print(f"Succesfully Scraped Members")
+
+
+client.run(TOKEN, bot=True)
